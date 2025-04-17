2efcf3dfc615a97a77607e34fcb10f6af28eb777
Ali Cheraghi <alichraghi@proton.me>
2025-04-17 21:10:46 +0330

    init

---
+++ content/blog/zig-gpu/kernel.zig
@@ -0,0 +1,15 @@
+// SPIR-V:
+//     zig build-obj -target spirv64-vulkan-none -mcpu vulkan_v1_2+int64 \
+//     -ofmt=spirv -fno-llvm kernel.zig
+//     zig build-obj -target spirv64-opencl-none -mcpu opencl_v2+int64 \
+//     -ofmt=spirv -fno-llvm kernel.zig
+//
+// PTX:
+//     zig build-lib -dynamic -target nvptx64-cuda-none -mcpu <gpu-model> \
+//     -fno-allow-shlib-undefined -fno-compiler-rt -fno-ubsan-rt kernel.zig
+//
+// AMDGCN:
+//     zig build-lib -dynamic -target amdgcn-amdhsa-none -mcpu <gpu-model> \
+//     -fno-allow-shlib-undefined -fno-compiler-rt -fno-ubsan-rt kernel.zig
+
+export fn kernel() callconv(.kernel) void {}
